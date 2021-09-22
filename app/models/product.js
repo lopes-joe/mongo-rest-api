e9f2f3cf204da9b4166e56d020d335a4c31aa67c
lopes-joe <slopes.joe@gmail.com>
2021-09-22 16:48:19 -0300

    created product class and a schema with Mongoose

---
+++ app/models/product.js
@@ -0,0 +1,29 @@
+/**
+ * Arquivo: produto.js
+ * Descrição: trata o modelo da classe produto
+ * Author: Joe Lopes de Souza
+ * Data: 22/09/2021
+ * 
+ */
+
+// o mongoose é usado para criar esquemas para persistência em banco de dados
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+/**
+ * PRODUTO
+ * 
+ * Id: int
+ * Nome: String
+ * Preço: Number
+ * Descricao: String
+ * 
+ */
+
+const productSchema = new Schema({
+    nome: String,
+    price: Number,
+    description: String
+})
+
+module.exports = mongoose.model('Product', productSchema)
